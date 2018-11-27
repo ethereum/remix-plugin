b5fbf1b78e74b8ab6555d1ecb7de94c3c5ae39b7
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2018-11-27 19:29:30 +0100

    Add a example using an iframe

--- examples/typescript-plugin/src/main.ts
+++ examples/typescript-plugin/src/main.ts
@@ -1,7 +1,35 @@
-import { RemixExtension } from 'remix-plugin'
+import { RemixPlugin, ModuleManager } from 'remix-plugin'
 
-const extension = new RemixExtension()
+/**
+ * A simple Plugin that creates an Iframe
+ */
+export class SimpleIframePlugin extends RemixPlugin {
 
-function getCompilation() {
+  protected manager: ModuleManager
 
+  constructor() {
+    super('simple')
+  }
+
+  /** When activated by the PluginManager */
+  async activate(manager: ModuleManager) {
+    this.manager = manager
+    // Init
+    const webview = await this.manager.request('webview', 'create', {
+      url: '',
+      hash: '',
+    })
+
+    webview.onMessage(message => {
+      // Do something with the message
+    })
+
+    // Export a method
+    this.addMethod('simpleMethod', async (value: string) => {
+      const message = { value }
+      webview.postMessage(message)
+    })
+  }
+
+  deactivate() {}
 }
