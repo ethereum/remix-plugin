bd7f06127947b7b1f0a689bb4f8777c01c648f24
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2018-12-02 17:14:03 +0100

    Direct relation with iframe and manager

--- src/hacking/iframe.module.ts
+++ src/hacking/iframe.module.ts
@@ -1,42 +1,122 @@
-import { IframeService } from './iframe.service'
+import { ModuleManager } from './module-manager'
 import { RemixModule, ModuleProfile, Message } from './remix-module'
 
 export interface IframeProfile extends ModuleProfile {
   url: string
+  load: { type: string, key: string }
 }
 
-export class IframePlugin extends RemixModule {
+export class IframeModule extends RemixModule {
   private id = 0
+  private iframe: HTMLIFrameElement
   private source: Window
   private origin: string
+  private pendingRequest: {
+    [type: string]: {
+      [id: number]: (value: any) => void
+    }
+  } = {}
+
+  public activate: () => void
+  public deactivate: () => void
 
   constructor(
     json: IframeProfile,
-    private service: IframeService
+    private manager: ModuleManager
   ) {
     super(json)
-    // Create the iframe
-    const { iframe, origin } = this.service.create(json)
-    this.source = iframe.contentWindow as Window
-    this.origin = origin
 
-    // Listen on Events
-    json.notifications.forEach(({ type, key }) => {
-      this.service.onEvent(json.type, type, key, (value: any) => {
-        const msg = JSON.stringify({ type, key, value })
-        this.source.postMessage(msg, this.origin)
+    const getMessage = (event) => this.getMessage(event)
+
+    // Listen on message from the iframe and to the event
+    this.activate = async () => {
+      await this.create(json)
+      window.addEventListener('message', getMessage, false)
+      json.notifications.forEach(({ type, key }) => {
+        this.manager.addEvent(json.type, type, key, (value: any) => {
+          this.postMessage({ type, key, value })
+        })
       })
-    })
+    }
+
+    // Remove events that come from iframe
+    this.deactivate = () => {
+      this.iframe.remove()
+      window.removeEventListener('message', getMessage, false)
+      json.notifications.forEach(({ type, key }) => {
+        this.manager.removeEvent(json.type, type, key)
+      })
+    }
   }
 
+  /** Post a message to the iframe of this plugin */
+  private postMessage(message: Partial<Message>) {
+    const msg = JSON.stringify(message)
+    this.source.postMessage(msg, this.origin)
+  }
+
+  /** Get message from the iframe */
+  private async getMessage(event: MessageEvent) {
+    const message = JSON.parse(event.data) as Message
+
+    if (event.origin !== this.origin) return	// Filter only messages that comes from this origin
+    switch (message.action) {
+      case 'notification' : {
+        this.manager.broadcast(message)
+        break
+      }
+      case 'request' : {
+        const action = 'response'
+        try {
+          const res = await this.manager.call(message)
+          this.postMessage({ ...res, action  })
+        } catch (error) {
+          this.postMessage({ ...message, action, error})
+        }
+        break
+      }
+      case 'response': {
+        const { type, id, value } = message
+        this.pendingRequest[type][id](value)
+        delete this.pendingRequest[type][id]
+        break
+      }
+      default: {
+        throw new Error('Message should be a notification, request or response')
+      }
+    }
+  }
+
+  /** Create an iframe element */
+  private async create({url, load}: IframeProfile) {
+    // Create
+    try {
+      const { type, key } = load
+      const message = { action: 'request', type, key, value: {} } as Message
+      const parent = await this.manager.call(message) as HTMLElement
+      this.iframe = document.createElement('iframe')
+      this.iframe.src = url
+      parent.appendChild(this.iframe)
+      if (!this.iframe.contentWindow) throw new Error('No window attached to Iframe')
+      this.source = this.iframe.contentWindow
+      this.origin = this.iframe.contentWindow.origin
+    } catch (err) {
+      console.log(err)
+    }
+
+    // Handshake
+    this.postMessage({ action: 'request', type: this.type, key : 'handshake' })
+  }
+
+
   /** Call a method of this plugin */
-  call(message: Message) {
+  public call(message: Message) {
     this.checkMethod(message)
     this.id ++
     const msg = JSON.stringify({ id: this.id, ...message })
     this.source.postMessage(msg, this.origin)
     return new Promise((res, rej) => {
-      this.service.onResponse(this.type, this.id, (value: any) => res(value))
+      this.pendingRequest[this.type][this.id] = (value: any) => res(value)
     })
   }
 
--- src/hacking/module-manager.ts
+++ src/hacking/module-manager.ts
@@ -19,9 +19,19 @@ export class ModuleManager {
     this.modules[module.type] = module
   }
 
+  /** Remove a module from the module manager */
+  public removeModule(module: RemixModule) {
+    delete this.modules[module.type]
+  }
+
   /** Add an event listen from a module to another */
   public addEvent(origin: string, type: string, key: string, cb: (value: any) => any) {
     this.events[origin][type][key] = cb
+  }
+
+  /** Remove an event listen from a module */
+  public removeEvent(origin: string, type: string, key: string) {
+    delete this.events[origin][type][key]
   }
 
   /** Call a specific module */
