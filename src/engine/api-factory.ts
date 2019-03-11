bbe9307acccc4b6391e937743984c9be156e4166
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-03-11 15:38:55 +0100

    Put `addRequest` inside the `api()` method

--- src/engine/api-factory.ts
+++ src/engine/api-factory.ts
@@ -1,62 +1,47 @@
-import { ModuleProfile, Api } from "src/types"
-
-interface PluginRequest {
-  from: string
-}
+import {
+  ModuleProfile,
+  Api,
+  ApiEventEmitter,
+  PluginRequest,
+  ExtractKey,
+  PluginApi,
+} from 'src/types'
 
 export abstract class ApiFactory<T extends Api = any> {
-  readonly abstract profile: ModuleProfile<T>
+  abstract readonly profile: ModuleProfile<T>
+  abstract events: ApiEventEmitter<T>
   private requestQueue: Array<() => Promise<any>> = []
   protected currentRequest: PluginRequest
 
-  addRequest(request: PluginRequest, method: string, args: any[]) {
-    return new Promise((resolve, reject) => {
-      // Add a new request to the queue
-      this.requestQueue.push(async () => {
-        this.currentRequest = request
-        const result = await this[method](...args)
-        resolve(result)
-        // Remove current request and call next
-        this.requestQueue.shift()
-        if (this.requestQueue.length !== 0) this.requestQueue[0]()
-      })
-      // If there is only one request waiting, call it
-      if (this.requestQueue.length === 1) {
-        this.requestQueue[0]()
-      }
-    })
-  }
-
-  api() {
-    const api = {}
-    const methods = this.profile.methods || []
-    methods.forEach(method => api[method as string] = this[method as string])
+  public api(): PluginApi<T> {
     return {
-      api,
-      ...this.profile,
-      addRequest: (request: PluginRequest, method: string, args: any[]) => this.addRequest(request, method, args)
-    }
-  }
-}
-
-
-class AppManager {
-  calls = {}
-  // When a method is called we update checksecurity
-  activateModule(api) {
-    api.methods.forEach(method => {
-      this.calls[api.name][method] = async (request: PluginRequest, ...args: any[]) => {
-        const result = await api.addRequest(request, method, args)
-        return result
-      }
-    })
-  }
-
-  // When a plugin call a module, we pass the request object as argument
-  activatePlugin(plugin) {
-    plugin.callModule = ({ name, key, payload }) => {
-      const request = { from: plugin.name }
-      return this.calls[name][key](request, ...payload)
+      events: this.events,
+      name: this.profile.name,
+      profile: this.profile,
+      addRequest: (
+        request: PluginRequest,
+        method: ExtractKey<T, Function>,
+        args: any[],
+      ) => {
+        return new Promise((resolve, reject) => {
+          if (!this.profile.methods.includes(method)) {
+            reject(new Error(`Method ${method} is not exposed by ${this.profile.name}`))
+          }
+          // Add a new request to the queue
+          this.requestQueue.push(async () => {
+            this.currentRequest = request
+            const result = await this[method as string](...args)
+            resolve(result)
+            // Remove current request and call next
+            this.requestQueue.shift()
+            if (this.requestQueue.length !== 0) this.requestQueue[0]()
+          })
+          // If there is only one request waiting, call it
+          if (this.requestQueue.length === 1) {
+            this.requestQueue[0]()
+          }
+        })
+      },
     }
   }
 }
