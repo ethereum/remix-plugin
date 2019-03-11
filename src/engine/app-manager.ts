59f0ad9ec986af66b77411ed93ea89153f94c3b6
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-03-11 15:39:41 +0100

    Switch `Entry` to `PluginApi`

--- src/engine/app-manager.ts
+++ src/engine/app-manager.ts
@@ -6,20 +6,23 @@ import {
   Api,
   ApiEventEmitter,
   Entry,
+  PluginRequest,
+  PluginApi,
 } from '../types'
 import { EventEmitter } from 'events'
+import { Plugin } from './plugin'
 
 export interface AppManager extends Api {
   name: 'appManager'
   events: {
     register: string
-    activate: Entry<Api>
+    activate: PluginApi<Api>
     deactivate: ModuleProfile
   }
-  registerMany(entry: Entry<any>[]): void
-  registerMany(entry: Entry<any>[]): void
-  registerOne<T extends Api>(entry: Entry<T>): void
-  registerOne<T extends Api>(entry: Entry<T>): void
+  registerMany(entry: PluginApi<any>[]): void
+  registerMany(entry: PluginApi<any>[]): void
+  registerOne<T extends Api>(entry: PluginApi<T>): void
+  registerOne<T extends Api>(entry: PluginApi<T>): void
   activateMany(names: string[]): void
   deactivateMany(names: string[]): void
   activateOne(name: string): void
@@ -43,7 +46,7 @@ export abstract class AppManagerApi implements API<AppManager> {
   private eventmanager: EventListeners = {}
   private calls: {
     [name: string]: {
-      [key: string]: Function
+      [key: string]: (requestInfo: PluginRequest, ...payload: any[]) => any
     }
   } = {}
 
@@ -55,10 +58,10 @@ export abstract class AppManagerApi implements API<AppManager> {
   //////////////
 
   /** Method to implement: get a module from the state of the application */
-  abstract getEntity<T extends Api>(name: string): Entry<T>
+  abstract getEntity<T extends Api>(name: string): PluginApi<T>
 
   /** Method to implement: Should add the plugin or module to the state of the application */
-  abstract addEntity<T extends Api>(entry: Entry<T>): void
+  abstract addEntity<T extends Api>(api: PluginApi<T>): void
 
   /** Method to implement: Do something when module or plugin is activated or deactivated */
   abstract setActive(name: string, isActive: boolean): void
@@ -68,8 +71,8 @@ export abstract class AppManagerApi implements API<AppManager> {
   /////////////
 
   /** Check if profile is a plugin */
-  private isPlugin<T extends Api>(entry: Entry<T>): entry is PluginEntry<T> {
-    return entry.profile['url']
+  private isPlugin<T extends Api>(api: PluginApi<T>): api is Plugin<T> {
+    return api.profile['url']
   }
 
   //////////////
@@ -77,7 +80,7 @@ export abstract class AppManagerApi implements API<AppManager> {
   //////////////
 
   /** Register many Modules or Plugins and activate them */
-  public init(entries: Entry<any>[]) {
+  public init(entries: PluginApi<any>[]) {
     entries.forEach(entry => {
       entry.profile.required = true
       this.registerOne(entry)
@@ -86,12 +89,12 @@ export abstract class AppManagerApi implements API<AppManager> {
   }
 
   /** Register many Modules or Plugins */
-  public registerMany(entries: Entry<any>[]) {
+  public registerMany(entries: PluginApi<any>[]) {
     entries.forEach(entry => this.registerOne(entry))
   }
 
   /** Register on Module or Plugin */
-  public registerOne<T extends Api>(entry: Entry<T>) {
+  public registerOne<T extends Api>(entry: PluginApi<T>) {
     // Add a default location is provided by the AppManager update the profile with it
     if (!entry.profile['location'] && this['defaultLocation']) {
       entry.profile['location'] = this['defaultLocation']
@@ -111,57 +114,59 @@ export abstract class AppManagerApi implements API<AppManager> {
 
   /** Activate a module or plugin */
   public activateOne(name: string) {
-    const entry = this.getEntity(name)
-    this.activateCallAndEvent(entry)
-    if (this.isPlugin(entry)) {
-      this.activateRequestAndNotification(entry)
+    const api = this.getEntity(name)
+    this.activateCallAndEvent(api)
+    if (this.isPlugin(api)) {
+      this.activateRequestAndNotification(api)
     }
-    if (entry.api.activate) {
-      entry.api.activate()
+    if (api.activate) {
+      api.activate()
     }
     this.setActive(name, true)
-    this.events.emit('activate', entry)
+    this.events.emit('activate', api)
   }
 
   /** Activation for Module and Plugin */
-  private activateCallAndEvent<T extends Api>({ profile, api }: Entry<T>) {
-    const events = profile.events || []
-    events.forEach(event => {
+  private activateCallAndEvent<T extends Api>(api: PluginApi<T>) {
+    const events = api.profile.events || []
+    events.forEach((event: keyof T['events']) => {
       if (!api.events) return
       api.events.on(event, (...payload: any[]) =>
-        this.broadcast(profile.name, event as string, payload),
+        this.broadcast(api.name, event as string, payload),
       )
     })
 
-    const methods = profile.methods || []
-    this.calls[profile.name] = {}
+    const methods = api.profile.methods || []
+    this.calls[api.name] = {}
     methods.forEach(key => {
       if (key in api) {
-        this.calls[profile.name][key as string] = (...args: any[]) =>
-          api[key as string](...args)
+        this.calls[api.name][key as string] = async (
+          request: PluginRequest,
+          ...args: any[]
+        ) => {
+          return api.addRequest(request, key, args)
+        }
       }
     })
   }
 
   /** Activation for Plugin only */
-  private activateRequestAndNotification<T extends Api>({
-    profile,
-    api,
-  }: PluginEntry<T>) {
+  private activateRequestAndNotification<T extends Api>(api: Plugin<T>) {
     api.request = ({ name, key, payload }) => {
       if (!Array.isArray(payload)) payload = [payload]
-      return this.calls[name][key](...payload)
+      const requestInfo: PluginRequest = { from: api.name }
+      return this.calls[name][key](requestInfo, ...payload)
     }
 
-    const notifications = profile.notifications || {}
+    const notifications = api.profile.notifications || {}
     for (const name in notifications) {
-      const origin = profile.name
+      const origin = api.profile.name
       if (!this.eventmanager[origin]) this.eventmanager[origin] = {}
       if (!this.eventmanager[origin][name]) this.eventmanager[origin][name] = {}
       const keys = notifications[name] || []
-      keys.forEach(
-        key => (this.eventmanager[origin][name][key] = api.notifs[name][key]),
-      )
+      keys.forEach(key => {
+        this.eventmanager[origin][name][key] = api.notifs[name][key]
+      })
     }
   }
 
@@ -176,12 +181,12 @@ export abstract class AppManagerApi implements API<AppManager> {
 
   /** Deactivate a module or plugin */
   public deactivateOne(name: string) {
-    const { profile, api } = this.getEntity(name)
+    const api = this.getEntity(name)
     this.deactivateProfile(name)
     // if (api.events) api.events.removeAllListeners()
     if (api.deactivate) api.deactivate()
     this.setActive(name, false)
-    this.events.emit('deactivate', profile)
+    this.events.emit('deactivate', api.profile)
   }
 
   /** Deactivation for modules and plugins */
