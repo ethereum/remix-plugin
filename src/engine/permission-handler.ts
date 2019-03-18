0c13f4bfbb0e2363dc50d15a265ee623cfca0146
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-03-18 17:31:53 +0100

    Use profile in permission

--- src/engine/permission-handler.ts
+++ src/engine/permission-handler.ts
@@ -1,30 +1,36 @@
+import { PluginProfile, ModuleProfile } from 'src/types'
+
 export interface Permissions {
   [to: string]: {
     [from: string]: {
-      allow: boolean,
-      hash: string,
+      allow: boolean
+      hash: string
     }
   }
 }
 
 export interface IPermissionProvider {
-  confirm(message: string): Promise<{ allow: boolean, remember: boolean }>
-  /** Clear the permission object */
-  clear(): void
+  confirm(message: string): Promise<{ allow: boolean; remember: boolean }>
 }
 
 export interface IPermissionHandler {
+  /** The list of the current permissions */
   permissions: Permissions
-  askPermission(from: {name: string, hash: string}, to: string): Promise<boolean>
+  /** Ask the Permission to the user */
+  askPermission(from: PluginProfile, to: ModuleProfile): Promise<boolean>
+  /** Clear the permission object */
+  clear(): void
 }
 
 /**
  * Example of a PermissionHandler using localStorage
  */
-export abstract class PermissionHandler implements IPermissionHandler, IPermissionProvider {
-
+export abstract class PermissionHandler
+  implements IPermissionHandler, IPermissionProvider {
   public permissions: Permissions
-  abstract confirm(message: string): Promise<{ allow: boolean, remember: boolean }>
+  abstract confirm(
+    message: string,
+  ): Promise<{ allow: boolean; remember: boolean }>
 
   constructor() {
     this.permissions = JSON.parse(localStorage.getItem('permissions'))
@@ -46,9 +52,10 @@ export abstract class PermissionHandler implements IPermissionHandler, IPermissi
    * @param wasAllow Did the plugin have changed its hash
    */
   private async openPermission(
-    from: {name: string, hash: string},
-    to: string, wasAllow: boolean
-  ): Promise<{ allow: boolean, remember: boolean }> {
+    from: PluginProfile,
+    to: ModuleProfile,
+    wasAllow: boolean,
+  ): Promise<{ allow: boolean; remember: boolean }> {
     const msg = wasAllow
       ? `${from.name} would like to access plugin ${to}.\n Check it's new content ${from.hash}`
       : `${from.name} has changed and would like to access the plugin ${to}.\n Check it's new content ${from.hash}`
@@ -57,26 +64,35 @@ export abstract class PermissionHandler implements IPermissionHandler, IPermissi
 
   /**
    * Check if a plugin has the permission to call another plugin and askPermission if needed
-   * @param from The name and hash of the plugin that make the call
-   * @param to The name of the plugin that receive the call
+   * @param from the profile of the plugin that make the call
+   * @param to The profile of the module that receive the call
    */
-  public async askPermission(from: {name: string, hash: string}, to: string): Promise<boolean> {
-    if (!this.permissions[to]) this.permissions[to] = {}
+  public async askPermission(
+    from: PluginProfile,
+    to: ModuleProfile,
+  ): Promise<boolean> {
+    if (!this.permissions[to.name]) this.permissions[to.name] = {}
     // Never allowed
-    if (!this.permissions[to][from.name]) {
+    if (!this.permissions[to.name][from.name]) {
       const { allow, remember } = await this.openPermission(from, to, false)
-      if (remember) this.permissions[to][from.name] = { allow, hash: this.permissions[to][from.name].hash }
+      if (remember) {
+        const hash = this.permissions[to.name][from.name].hash
+        this.permissions[to.name][from.name] = { allow, hash }
+      }
       this.setPermissions()
       return allow
     }
     // Remember not allow
-    if (!this.permissions[to][from.name].allow) {
+    if (!this.permissions[to.name][from.name].allow) {
       return false
     }
     // Remember allow but hash has changed
-    if (this.permissions[to][from.name].hash !== from.hash) {
+    if (this.permissions[to.name][from.name].hash !== from.hash) {
       const { allow, remember } = await this.openPermission(from, to, true)
-      if (remember) this.permissions[to][from.name] = { allow, hash: this.permissions[to][from.name].hash }
+      if (remember) {
+        const hash = this.permissions[to.name][from.name].hash
+        this.permissions[to.name][from.name] = { allow, hash }
+      }
       this.setPermissions()
       return allow
     }
