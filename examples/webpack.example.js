3d396e8f19dabd31ee155a8a57f78e6f21ed4597
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-03-26 13:59:31 +0100

    Add plugin infrastructure for examples

---
+++ examples/webpack.example.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin({ configFile: path.resolve(__dirname, './tsconfig.example.json') })]
+  },
+}
