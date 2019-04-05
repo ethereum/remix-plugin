eecb797c8e70c8b299a0791f661c56e507158fe7
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-05 13:25:45 +0200

    Add editor

---
+++ src/api/editor/api.ts
@@ -0,0 +1,25 @@
+import { BaseApi, extendsProfile } from "../base"
+import { ModuleProfile, Api, API } from "../../types"
+import { HighlightPosition } from './type'
+
+export interface IEditorApi extends Api {
+  events: {
+  }
+  highlight(position: HighlightPosition, filePath: string, hexColor: string): void
+  discardHighlight(): void
+}
+
+export const compilerProfile: Partial<ModuleProfile<IEditorApi>> = {
+  kind: 'editor',
+  methods: ['highlight', 'discardHighlight']
+}
+
+export abstract class EditorApi extends BaseApi<IEditorApi> implements API<IEditorApi> {
+  constructor(profile: ModuleProfile) {
+    const localProfile = extendsProfile(profile, compilerProfile)
+    super(localProfile)
+  }
+
+  abstract highlight(position: HighlightPosition, filePath: string, hexColor: string): void
+  abstract discardHighlight(): void
+}
