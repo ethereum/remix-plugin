ac2885928efd4727c3c3ec51daec7ff69cab73e8
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-08 10:54:55 +0200

    Add network

---
+++ src/api/network/api.ts
@@ -0,0 +1,36 @@
+import { BaseApi, extendsProfile } from '../base'
+import { ModuleProfile, Api, API } from '../../types'
+import { Network, CustomNetwork, networkProvider } from './type'
+
+export interface INetworkApi extends Api {
+  events: {
+    providerChanged: [networkProvider]
+  }
+  getNetworkProvider(): networkProvider
+  detectNetwork(): Network | Partial<CustomNetwork>
+  getEndpoint(): string
+  addNetwork(network: CustomNetwork): void
+  removeNetwork(name: string): void
+}
+
+export const networkProfile: Partial<ModuleProfile<INetworkApi>> = {
+  kind: 'network',
+  events: ['providerChanged'],
+  methods: ['getNetworkProvider', 'getEndpoint', 'detectNetwork', 'addNetwork', 'removeNetwork'],
+}
+
+export abstract class NetworkApi extends BaseApi<INetworkApi>
+  implements API<INetworkApi> {
+  constructor(profile: ModuleProfile) {
+    const localProfile = extendsProfile(profile, networkProfile)
+    super(localProfile)
+  }
+
+  /** Return the current network provider */
+  abstract getNetworkProvider(): networkProvider
+  abstract detectNetwork(): Network | Partial<CustomNetwork>
+  /** Return the url only if network provider is 'web3' */
+  abstract getEndpoint(): string
+  abstract addNetwork(network: CustomNetwork): any
+  abstract removeNetwork(): any
+}
