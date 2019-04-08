b40e89f920999346517bfb21cc34e3d8bea6870c
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-08 14:53:40 +0200

    Add file system api

---
+++ src/api/fileSystem/api.ts
@@ -0,0 +1,30 @@
+import { BaseApi, extendsProfile } from "../base"
+import { ModuleProfile, Api, API } from "../../types"
+import { Folder } from './type'
+
+export interface IFileSystemApi extends Api {
+  events: {
+    currentFileChanged: [string]
+  }
+  getFolder(path: string): Folder
+  getCurrentFile(): string
+  getFile(path: string): string
+  setFile(path: string, content: string): void
+}
+
+export const editorProfile: Partial<ModuleProfile<IFileSystemApi>> = {
+  kind: 'fs',
+  methods: ['getFolder', 'getCurrentFile', 'getFile', 'setFile']
+}
+
+export abstract class FileSystemApi extends BaseApi<IFileSystemApi> implements API<IFileSystemApi> {
+  constructor(profile: ModuleProfile) {
+    const localProfile = extendsProfile(profile, editorProfile)
+    super(localProfile)
+  }
+
+  abstract getFolder(path: string): Folder
+  abstract getCurrentFile(): string
+  abstract getFile(path: string): string
+  abstract setFile(path: string, content: string): void
+}
