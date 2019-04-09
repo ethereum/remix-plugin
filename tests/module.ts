d9260e08af174492d368837322d2ec990d29f50c
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-09 10:40:06 +0200

    fix tests

--- tests/module.ts
+++ tests/module.ts
@@ -19,7 +19,7 @@ describe('Module', () => {
     expect(app['calls'][compiler.name]).toBeDefined()
   })
   test('method is added to app', () => {
-    expect(app['calls'][compiler.name]['lastCompilationResult']).toBeDefined()
+    expect(app['calls'][compiler.name]['getCompilationResult']).toBeDefined()
   })
   test('deactivate module', () => {
     app.deactivateOne(compiler.name)
@@ -27,30 +27,30 @@ describe('Module', () => {
   })
 
   test('can call a method', async () => {
-    const result = await app['calls'][compiler.name].lastCompilationResult({from: AUTHORIZED})
+    const result = await app['calls'][compiler.name].getCompilationResult({from: AUTHORIZED})
     expect(result).toBe('compilation')
   })
   test('get current request', async () => {
     const requestInfo = {from: AUTHORIZED}
-    await app['calls'][compiler.name].lastCompilationResult(requestInfo)
+    await app['calls'][compiler.name].getCompilationResult(requestInfo)
     expect(compiler['currentRequest']).toBe(requestInfo)
   })
   test('handle error in calls', async () => {
     try {
       const requestInfo = {from: UNAUTHORIZED}
-      await app['calls'][compiler.name].lastCompilationResult(requestInfo)
+      await app['calls'][compiler.name].getCompilationResult(requestInfo)
     } catch (err) {
       expect(err.message).toMatch(/not allowed/)
     }
   })
   test('calls are handled in queue', async () => {
     app['calls'][compiler.name]
-      .lastCompilationResult({from: AUTHORIZED})
+      .getCompilationResult({from: AUTHORIZED})
       .then(result => expect(result).toBe('compilation'))
-    app['calls'][compiler.name].lastCompilationResult({from: UNAUTHORIZED})
+    app['calls'][compiler.name].getCompilationResult({from: UNAUTHORIZED})
       .catch(err => expect(err.message).toMatch(/not allowed/))
     app['calls'][compiler.name]
-      .lastCompilationResult({from: AUTHORIZED})
+      .getCompilationResult({from: AUTHORIZED})
       .then(result => expect(result).toBe('compilation'))
   })
 })
