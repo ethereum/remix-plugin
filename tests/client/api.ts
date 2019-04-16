3b0de1d886072c0b1029d0c8e06a8709c26b911b
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-16 13:57:00 +0200

    Add Tests for API

--- tests/client/api.ts
+++ tests/client/api.ts
@@ -1,11 +1,13 @@
-import { createApi, ModuleProfile, PluginClient, CustomApi, Api } from '../../src'
+import { createApi, ModuleProfile, PluginClient, CustomApi, Api, callEvent, listenEvent, getApiMap, Theme, listenOnThemeChanged } from '../../src'
 
 interface TestApi extends Api {
   name: 'test',
   events: {
     event: (isTrue: boolean) => void
   }
-  method: (isTrue: boolean) => boolean
+  methods: {
+    method: (isTrue: boolean) => boolean
+  }
 }
 
 const profile: ModuleProfile<TestApi> = {
@@ -19,16 +21,70 @@ describe('Client Api', () => {
   let api: CustomApi<TestApi>
   beforeEach(() => {
     client = new PluginClient()
+    client['loaded'] = true
     api = createApi(client, profile)
   })
 
   test('Should create an Api', () => {
     expect(api).toBeDefined()
-    expect(api.event).toBeDefined()
+    expect(api.on).toBeDefined()
     expect(api.method).toBeDefined()
   })
 
-  test('Method should be a promise', (done) => {
-    api.method
+  test('"Method" should send a message', (done) => {
+    client.events.on('send', message => {
+      expect(message).toEqual({
+        action: 'request',
+        name: 'test',
+        key: 'method',
+        payload: [true],
+        id: 1
+      })
+      done()
+    })
+    api.method(true)
+  })
+
+  test('"Method" should return a promise', (done) => {
+    client.events.on('send', ({name, key, id, payload}) => {
+      client.events.emit(callEvent(name, key, id), payload[0])
+    })
+    api.method(true).then(isTrue => {
+      expect(isTrue).toBeTruthy()
+      done()
+    })
+  })
+
+  test('"Event" should emit an event', (done) => {
+    api.on('event', (isTrue) => {
+      expect(isTrue).toBeTruthy()
+      done()
+    })
+    client.events.emit(listenEvent('test', 'event'), true)
+  })
+
+  test('getApiMap returns a map of API', () => {
+    const { test } = getApiMap(client, [profile])
+    expect(test.on).toBeDefined()
+    expect(test.method).toBeDefined()
+  })
+})
+
+/////////////////
+// COMMON APIS //
+/////////////////
+
+describe('Common Apis', () => {
+  let client: PluginClient
+  beforeEach(() => {
+    client = new PluginClient()
+  })
+  test('Should listen on theme changed', () => {
+    const link = listenOnThemeChanged(client, { customTheme: false }) as HTMLLinkElement
+    expect(link.getAttribute('rel')).toBe('stylesheet')
+    client.events.emit('themeChanged', { url: 'url', quality: 'dark'})
+    setTimeout(() => {
+      expect(link.getAttribute('href')).toBe('url')
+    }, 100)
   })
 })
