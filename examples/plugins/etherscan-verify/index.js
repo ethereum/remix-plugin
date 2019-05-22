b305ac83833627aba08b11df4967479db92f7958
yann300 <yann.levreau@gmail.com>
2019-05-22 16:47:34 +0200

    fix etherscan plugin example

--- examples/plugins/etherscan-verify/index.js
+++ examples/plugins/etherscan-verify/index.js
@@ -15,6 +15,15 @@ client.on(
   },
 )
 
+const apikeyStorageKey = 'etherscan-api-key'
+function saveAPIkey (e) {
+  const value = document.querySelector('input#apikey').value
+  localStorage.setItem(apikeyStorageKey, value)
+  apikey = value
+}
+let apikey  = localStorage.getItem(apikeyStorageKey)
+if (apikey) document.querySelector('input#apikey').value = apikey
+
 // Get the current compilation result and make a request to the Ehterscan API
 async function getResult() {
   const el = document.querySelector('div#results')
@@ -54,8 +63,10 @@ async function doPost(info, address) {
   const name = document.getElementById('verifycontractname').value
   let contractMetadata = info.data.contracts[fileName][name]['metadata']
   contractMetadata = JSON.parse(contractMetadata)
+  const ctrArgument = document.getElementById('verifycontractarguments').value ?
+  document.getElementById('verifycontractarguments').value.replace('0x', '') : ''
   const data = {
-    apikey: 'CC4YVQGTC45H2IXX6BDUNP54TXJHKVMWY5', //A valid API-Key is required
+    apikey: apikey, //A valid API-Key is required
     module: 'contract', //Do not change
     action: 'verifysourcecode', //Do not change
     contractaddress: address, //Contract Address starts with 0x...
@@ -64,19 +75,57 @@ async function doPost(info, address) {
     compilerversion: `v${contractMetadata.compiler.version}`, // see http://etherscan.io/solcversions for list of support versions
     optimizationUsed: contractMetadata.settings.optimizer.enabled ? 1 : 0, //0 = Optimization used, 1 = No Optimization
     runs: contractMetadata.settings.optimizer.runs, //set to 200 as default unless otherwise
-    constructorArguements: document.getElementById('verifycontractarguments')
-      .value, //if applicable
+    constructorArguements: ctrArgument, //if applicable
+  }
+  let formData = new FormData()
+  for (var k in data) {
+    formData.append(k, data[k])
   }
 
   try {
+    client.emit('statusChanged', { key: 'loading', type: 'info', title: 'Verifying ...' })
     const response = await fetch(etherscanApi, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json; charset=utf-8' },
-      body: data,
+      body: formData
     })
     const json = await response.json()
+    if (json.message === 'OK' && json.status === '1') checkValidation(etherscanApi, json.result)
+    if (json.message === 'NOTOK') {
+      client.emit('statusChanged', { key: 'failed', type: 'error', title: json.result })
+      scheduleResetStatus()
+    }
     return json.result
   } catch (error) {
     document.querySelector('div#results').innerText = error
   }
+}
+
+async function checkValidation (etherscanApi, guid) {
+  try {
+    let params = `?guid=${guid}&module=contract&action=checkverifystatus`
+    const response = await fetch(etherscanApi + params, {
+      method: 'GET'
+    })
+    const json = await response.json()
+    document.querySelector('div#results').innerText = json.message + ' ' + json.result
+    if (json.message === 'NOTOK' && json.result === 'Pending in queue') {
+      setTimeout(() => {
+        document.querySelector('div#results').innerText += '. Polling...'
+        checkValidation(etherscanApi, guid)
+      }, 4000);
+    } else  if (json.message === 'OK') {
+      client.emit('statusChanged', { key: 'succeed', type: 'success', title: json.result + ' Verified!' })
+    } else {
+      client.emit('statusChanged', { key: 'failed', type: 'error', title: json.result })
+    }
+    scheduleResetStatus()
+  } catch (error) {
+    document.querySelector('div#results').innerText = error
+  }
+}
+
+function scheduleResetStatus () {
+  setTimeout(() => {
+    client.emit('statusChanged', { key: 'none' })
+  }, 10000)
 }
