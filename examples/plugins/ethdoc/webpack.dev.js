993f5bf39a55fae9717047bd6073f3aa0f83062e
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-07-23 15:30:13 +0200

    upgrade build config ethdoc

--- examples/plugins/ethdoc/webpack.dev.js
+++ examples/plugins/ethdoc/webpack.dev.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const merge = require('webpack-merge');
-// const common = require('../../webpack.example');
 const common = require('../../../webpack.common');
 
 module.exports = merge(common, {
