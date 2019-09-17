af5eda804e01d8720eb61a9f716a6dd09d51e2d4
yann300 <yann.levreau@gmail.com>
2019-09-17 14:29:19 +0200

    better syntax

--- projects/engine/src/plugin/util.ts
+++ projects/engine/src/plugin/util.ts
@@ -1,10 +1,9 @@
 
-const defaultGateways = {
+export const defaulGateways = {
     'ipfs://': 'https://ipfsgw.komputing.org/ipfs/',
     'swarm://': 'https://swarm-gateways.net/bzz-raw://'
 }
 
-export { defaultGateways }
 export function transformUrl (url: string) {
     const network = Object.keys(defaultGateways).find(key => url.startsWith(key))
     return network ? url.replace(network, defaultGateways[network]) : url
