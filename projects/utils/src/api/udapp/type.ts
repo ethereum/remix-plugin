ba1d1b897ff89152388080e865e7743a1d5a4822
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-09-30 17:13:30 +0200

    update type for RemixTx

--- projects/utils/src/api/udapp/type.ts
+++ projects/utils/src/api/udapp/type.ts
@@ -1,11 +1,38 @@
-export interface RemixTx {
-  from: string
-  to: string
-  gasLimit: string
+export type RemixTx = {
+  contractAddress: string
   data: string
+  envMode: 'vm'
+  executionCost: string
+  from: string
+  gas: string
+  hash: string
+  input: string
+  logs: any[]
+  returnValue: Uint8Array
+  status: '0x01' | '0x00'
+  transactionCost: string
+  transactionHash: string
   value: string
-  useCall: boolean
+} | {
+  blockHash: string
+  blockNumber: number
+  envMod: 'injected' | 'web3'
+  from: string
+  gas: number
+  gasPrice: { c: number[], e: number, s: number }
+  hash: string
+  input: string
+  none: number
+  r: string
+  s: string
+  v: string
+  status: '0x01' | '0x00'
+  to: string
+  transactionCost: string
+  transactionIndex: number
+  value: { c: number[], e: number, s: number }
 }
+
 
 export interface RemixTxReceipt {
   transactionHash: string
