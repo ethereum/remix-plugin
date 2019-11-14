82b5ff46ff20e7d258f1529d6401cac6f251466b
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-11-14 14:14:48 +0100

    Removed events

--- projects/utils/src/api/git/profile.ts
+++ projects/utils/src/api/git/profile.ts
@@ -4,5 +4,4 @@ import { LibraryProfile } from '../../types'
 export const gitProfile: LibraryProfile<IGitSystem> = {
   name: 'git',
   methods: ['clone', 'checkout', 'init', 'add', 'commit', 'fetch', 'pull', 'push', 'reset', 'status', 'remote', 'log']
-  //events: ['currentFileChanged']
 }
