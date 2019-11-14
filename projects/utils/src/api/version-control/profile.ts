35237848b379b16ed8cf2d96517f922f67b02136
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-11-14 14:06:42 +0100

    Renaming version control to git

--- projects/utils/src/api/version-control/profile.ts
+++
@@ -1,8 +0,0 @@
-import { IVersionControllSystem } from './api'
-import { LibraryProfile } from '../../types'
-
-export const versionControllProfile: LibraryProfile<IVersionControllSystem> = {
-  name: 'vc',
-  methods: ['gitClone', 'gitCheckout', 'gitInit', 'gitAdd', 'gitCommit', 'gitFetch', 'gitPull', 'gitPush', 'gitReset', 'gitStatus', 'gitRemote', 'gitLog']
-  //events: ['currentFileChanged']
-}
