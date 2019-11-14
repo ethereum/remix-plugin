44855f7c37775f35b05cdcb677ac0c52b7a1e132
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-11-14 14:19:29 +0100

    Renaming plugin from git to remixd.git

--- projects/utils/src/api/git/profile.ts
+++ projects/utils/src/api/git/profile.ts
@@ -2,6 +2,6 @@ import { IGitSystem } from './api'
 import { LibraryProfile } from '../../types'
 
 export const gitProfile: LibraryProfile<IGitSystem> = {
-  name: 'git',
+  name: 'remixd.git',
   methods: ['clone', 'checkout', 'init', 'add', 'commit', 'fetch', 'pull', 'push', 'reset', 'status', 'remote', 'log']
 }
