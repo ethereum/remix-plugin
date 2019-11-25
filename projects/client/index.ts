10f972cda151bed1d24dbd2e46b4c3089b42c91b
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-25 15:36:14 +0100

    Add service tests + update after tests

--- projects/client/index.ts
+++ projects/client/index.ts
@@ -1,4 +1,6 @@
 export * from './src/api'
 export * from './src/client'
 export * from './src/iframe'
+export * from './src/service'
+export * from './src/node'
 export * from './../utils/src/external-api'
