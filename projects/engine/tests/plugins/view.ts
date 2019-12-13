5205313fd21d8456eb1b39c341bed154f7865719
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-12-13 16:36:14 +0100

    Add tests for view & host

---
+++ projects/engine/tests/plugins/view.ts
@@ -0,0 +1,60 @@
+import { ViewPlugin } from '../../src/plugin/view'
+import { PluginManager } from '../../src/plugin/manager'
+import { Engine } from '../../src/engine/next-gen-engine'
+import { pluginManagerProfile } from '../../../utils'
+import { HostPlugin } from '@remixproject/engine'
+
+export class MockEngine extends Engine {
+  onRegistration = jest.fn()
+}
+
+class MockManager extends PluginManager {
+  constructor() {
+    super(pluginManagerProfile)
+  }
+}
+
+class MockHost extends HostPlugin {
+  focus = jest.fn() // (name: string) =>
+  addView = jest.fn() // (profile: Profile) =>
+  removeView = jest.fn() // (name: string) =>
+  constructor() {
+    super({ name: 'sidePanel', methods: [] })
+  }
+}
+
+class MockView extends ViewPlugin {
+  render = jest.fn(() => ({ id: 'element' } as Element))
+  onActivation = jest.fn()
+  onDeactivation = jest.fn()
+  constructor() {
+    super({ name: 'view', methods: [], location: 'sidePanel' })
+  }
+}
+
+describe.only('View Plugin', () => {
+  let manager: MockManager
+  let host: MockHost
+  let view: MockView
+  beforeEach(async () => {
+    view = new MockView()
+    host = new MockHost()
+    manager = new MockManager()
+    const engine = new MockEngine(manager)
+    await engine.onload()
+    engine.register([view, host])
+  })
+
+  test('Activate View call host addView', async () => {
+    await manager.activatePlugin(['sidePanel', 'view'])
+    expect(host.addView).toHaveBeenCalledWith(view.profile, { id: 'element' })
+    expect(view.onActivation).toHaveBeenCalledTimes(1)
+  })
+
+  test('Deactive View call host removeView', async () => {
+    await manager.activatePlugin(['sidePanel', 'view'])
+    await manager.deactivatePlugin('view')
+    expect(host.removeView).toHaveBeenCalledWith(view.profile)
+    expect(view.onDeactivation).toHaveBeenCalledTimes(1)
+  })
+})
