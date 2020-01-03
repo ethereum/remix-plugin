e54d82ade90a39f47d2c45145557f49d195d4ac4
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-03 15:49:47 +0100

    add tests for plugin-node

--- projects/utils/tests/method-path.ts
+++ projects/utils/tests/method-path.ts
@@ -1,4 +1,4 @@
-import { getMethodPath } from '../src/method-path'
+import { getMethodPath, getRootPath } from '../src/method-path'
 
 describe('Get Method Path', () => {
   test('call event', () => {
@@ -7,6 +7,15 @@ describe('Get Method Path', () => {
     expect(getMethodPath('call', 'remixd')).toEqual('call')
     expect(getMethodPath('call', 'remixd.cmd')).toEqual('cmd.call')
     expect(getMethodPath('call', 'remixd.cmd.git')).toEqual('cmd.git.call')
+  })
+
+})
+
+describe('Get Root Path', () => {
+  test('Get root', () => {
+    expect(getRootPath('remixd')).toEqual('remixd')
+    expect(getRootPath('remixd.cmd')).toEqual('remixd')
+    expect(getRootPath('remixd.cmd.git')).toEqual('remixd')
   })
 
 })
