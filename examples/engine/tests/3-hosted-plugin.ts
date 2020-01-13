9afd758e1272afeedf03ba5cabf7ffa9e70343aa
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-13 14:54:44 +0100

    Add doc for external plugin

--- examples/engine/tests/3-hosted-plugin.ts
+++ examples/engine/tests/3-hosted-plugin.ts
@@ -1,57 +1,58 @@
 import { PluginManager, Engine, HostPlugin, ViewPlugin } from '@remixproject/engine'
 import { Profile } from '@utils'
 
+// Host plugin display
+export class SidePanel extends HostPlugin {
+  plugins: Record<string, HTMLElement> = {}
+  focused: string
+  root: Element // The root html element of the HostPlugin
+  constructor(root = document.body) {
+    // Expose the 3 absract methods 'focus', 'addView', 'removeView'
+    super({ name: 'sidePanel', methods: ['focus', 'addView', 'removeView']})
+    this.root = root
+  }
+  // Add a View plugin into the DOM once it's activated
+  addView(profile: Profile, view: HTMLElement) {
+    this.plugins[profile.name] = view
+    view.style.display = 'none'
+    this.root.appendChild(view)
+  }
+  // Remove a View plugin from the DOM once it's deactivated
+  removeView(profile: Profile) {
+    this.root.removeChild(this.plugins[profile.name])
+    if (this.focused === profile.name) delete this.focused
+    delete this.plugins[profile.name]
+  }
+  // Set focus on one plugin
+  focus(name: string) {
+    if (this.plugins[name]) {
+      if (this.plugins[this.focused]) this.plugins[this.focused].style.display = 'none'
+      this.plugins[name].style.display = 'block'
+      this.focused = name
+    }
+  }
+}
+
+// View Plugin
+class HostedPlugin extends ViewPlugin {
+  root: HTMLElement
+  constructor() {
+    // Specific the location where this plugin is hosted
+    super({ name: 'hosted', location: 'sidePanel' })
+  }
+  // Render the element into the host plugin
+  render(): Element {
+    if (!this.root) {
+      this.root = document.createElement('div')
+    }
+    return this.root
+  }
+}
+
+
 test('[Example] Hosted Plugin', async () => {
 
   ///////////////////////////////////
-
-  // Host plugin display
-  class SidePanel extends HostPlugin {
-    plugins: Record<string, HTMLElement> = {}
-    focused: string
-    root: Element // The root html element of the HostPlugin
-    constructor(root = document.body) {
-      // Expose the 3 absract methods 'focus', 'addView', 'removeView'
-      super({ name: 'sidePanel', methods: ['focus', 'addView', 'removeView']})
-      this.root = root
-    }
-    // Add a View plugin into the DOM once it's activated
-    addView(profile: Profile, view: HTMLElement) {
-      this.plugins[profile.name] = view
-      view.style.display = 'none'
-      this.root.appendChild(view)
-    }
-    // Remove a View plugin from the DOM once it's deactivated
-    removeView(profile: Profile) {
-      this.root.removeChild(this.plugins[profile.name])
-      if (this.focused === profile.name) delete this.focused
-      delete this.plugins[profile.name]
-    }
-    // Set focus on one plugin
-    focus(name: string) {
-      if (this.plugins[name]) {
-        if (this.plugins[this.focused]) this.plugins[this.focused].style.display = 'none'
-        this.plugins[name].style.display = 'block'
-        this.focused = name
-      }
-    }
-  }
-
-  class HostedPlugin extends ViewPlugin {
-    root: HTMLElement
-    constructor() {
-      // Specific the location where this plugin is hosted
-      super({ name: 'hosted', location: 'sidePanel' })
-    }
-    // Render the element into the host plugin
-    render(): Element {
-      if (!this.root) {
-        this.root = document.createElement('div')
-      }
-      return this.root
-    }
-  }
-
 
   const manager = new PluginManager()
   const engine = new Engine(manager)
