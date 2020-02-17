4a4dd7ad7181676799a64ab44b17192cebec68e3
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-17 14:26:51 +0100

    update HostPlugin

--- projects/engine/src/plugin/host.ts
+++ projects/engine/src/plugin/host.ts
@@ -5,12 +5,15 @@ export abstract class HostPlugin extends Plugin {
 
   constructor(profile: Profile) {
     // Remove duplicated if needed
-    const methods = Array.from(new Set([...(profile.methods || []), 'focus', 'addView', 'removeView']))
+    const methods = Array.from(new Set([
+      ...(profile.methods || []),
+      'currentFocus', 'focus', 'addView', 'removeView'
+    ]))
     super({...profile, methods })
   }
 
-  /** Verify if a view plugin is focused */
-  abstract isFocus(name: string): boolean
+  /**  Give the name of the current focus plugin */
+  abstract currentFocus(): string
 
   /** Display the view inside the host */
   abstract focus(name: string): void
