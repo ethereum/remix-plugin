837142dc1a91cc0f7f73fd5a656316897eef2dfa
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-17 17:15:40 +0100

    Remove old example

--- examples/ide/src/plugins/compiler.plugin.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin, ICompiler, API, CompilationResult } from '@remixproject/engine'
-import { EngineApi } from './engine.api'
-
-export class CompilerPlugin extends Plugin<ICompiler, EngineApi> implements API<ICompiler> {
-  getCompilationResult(): CompilationResult {
-    return {} as CompilationResult
-  }
-  compile(fileName: string): void {}
-}
