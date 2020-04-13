d6d56da777dfe968065ab814160ac55701e881c9
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-04-13 16:56:59 +0200

    Add websocket connector

---
+++ projects/client-ws/connector.ts
@@ -0,0 +1,66 @@
+import { ClientConnector, connectClient, applyApi, Client } from '@remixproject/plugin/connector'
+import { PluginClient } from '@remixproject/plugin/client'
+import { Message } from '../utils/src/types/message'
+import { RemixApi } from '../utils/src/api/remix-profile'
+import { Api, ApiMap } from '../utils/src/types/api'
+
+
+export interface WS {
+  send(data: string): void
+  on(type: 'message', cb: (event: string) => any): this
+}
+
+/**
+ * This Websocket connector works with the library `ws`
+ */
+export class WebsocketConnector implements ClientConnector {
+
+  constructor(private websocket: WS) {}
+
+  /** Send a message to the engine */
+  send(message: Partial<Message>) {
+    this.websocket.send(JSON.stringify(message))
+  }
+
+  /** Get messae from the engine */
+  on(cb: (message: Partial<Message>) => void) {
+    this.websocket.on('message', (event) => cb(JSON.parse(event)))
+  }
+}
+
+/**
+ * Connect an Iframe client to a web engine
+ * @param client An optional iframe client to connect to the engine.
+ *
+ * ---------
+ * @example
+ * ```typescript
+ * const wss = new WebSocket.Server({ port: 8080 });
+ * wss.on('connection', (ws) => {
+ *  const client = createWebsocketClient(ws, new MyPlugin())
+ * })
+ * ```
+ * ---------
+ * @example
+ * ```typescript
+ * class MyPlugin extends PluginClient {
+ *  methods = ['hello']
+ *  hello() {
+ *   console.log('Hello World')
+ *  }
+ * }
+ * const wss = new WebSocket.Server({ port: 8080 });
+ * wss.on('connection', (ws) => {
+ *  const client = createWebsocketClient(ws, new MyPlugin())
+ * })
+ * ```
+ */
+export const createWebsocketClient = <
+  P extends Api,
+  App extends ApiMap = RemixApi
+>(websocket: WS, client: PluginClient<P, App> = new PluginClient()): Client<P, App> => {
+  const c = client as any
+  connectClient(new WebsocketConnector(websocket), c)
+  applyApi(c)
+  return c
+}
