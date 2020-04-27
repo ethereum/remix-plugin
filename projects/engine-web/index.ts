b27cbdd17ef1ea22181c1962d810597788cbf321
François <francois.guezengar@hotmail.fr>
2020-04-27 10:58:00 +0200

    Revert "add engine-web"
    This reverts commit 73cf976e12e9bb0ed854dd25026f1d22a4a0dd22.

--- projects/engine-web/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/ws'
-export * from './src/iframe'
--- projects/engine-web/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'engine-web',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/engine-web'
-};
