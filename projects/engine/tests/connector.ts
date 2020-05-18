ec763ff663338f57bf159672dcac279b93cb6ef0
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-18 18:11:59 +0200

    add test for connector

--- projects/engine/tests/connector.ts
+++ projects/engine/tests/connector.ts
@@ -1,58 +1,91 @@
-describe.skip('', () => {
-  // // Get Message: request
-  // test('[request] shoud trigger "call"', () => {
-  //   const spy = spyOn(iframe, 'send' as any)
-  //   iframe['origin'] = 'url'
-  //   const event = {
-  //     origin: 'url',
-  //     data: { id: 0, action: 'request', key: 'method', payload: ['params'], name: 'manager' }
-  //   }
-  //   iframe['getEvent'](event as any)
-  //   expect(iframe['getMessage']).toHaveBeenCalledWith({ id: 0, action: 'request', key: 'method', payload: ['params'], name: 'manager' })
-  //   const response = { id: 0, action: 'response', key: 'method', payload: [true], name: 'manager', error: undefined }
-  //   setTimeout(() => expect(spy).toHaveBeenCalledWith(response), 10)  // Wait for next tick
-  // })
+import { Engine } from '../src/engine'
+import { PluginConnector } from '../src/connector'
+import { PluginManager } from '../src/plugin/manager'
+import { pluginManagerProfile } from "../../utils"
 
-  // // Get Message: listen
-  // test('getEvent with listen should run "on"', () => {
-  //   const spy = spyOn(iframe, 'send' as any)
-  //   iframe['origin'] = 'url'
-  //   const event = {
-  //     origin: 'url',
-  //     data: { id: 0, action: 'listen', key: 'method', payload: ['params'], name: 'manager' }
-  //   }
-  //   iframe['getEvent'](event as any)
-  //   expect(iframe.on.mock.calls[0][0]).toEqual('manager')
-  //   expect(iframe.on.mock.calls[0][1]).toEqual('method')
-  //   iframe.callMockEvent(true)
-  //   iframe.callMockEvent(true)
-  //   iframe.callMockEvent(true)
-  //   const response = { action: 'notification', key: 'method', payload: [true], name: 'manager' }
-  //   expect(spy).toHaveBeenCalledWith(response)
-  //   expect(spy).toHaveBeenCalledTimes(3)
-  // })
+class MockPlugin extends PluginConnector {
+  callMockEvent: (...payload: any[]) => any
+  send = jest.fn()
+  connect = jest.fn()
+  disconnect = jest.fn()
+  call: jest.Mock
+  on: jest.Mock
+  once: jest.Mock
+  off: jest.Mock
+  emit: jest.Mock
+  constructor() {
+    super({ name: 'connector', url: 'url' })
+  }
+  createMock() {
+    this.call = jest.fn()
+    this.on = jest.fn((name, method, cb) => this.callMockEvent = (...payload) => cb(...payload))
+    this.once = jest.fn()
+    this.off = jest.fn()
+    this.emit = jest.fn()
+  }
+}
 
-  // // Get Message: once
-  // test('getEvent with once should run listen only one', () => {
-  //   const spy = spyOn(iframe, 'send' as any)
-  //   iframe['origin'] = 'url'
-  //   const event = {
-  //     origin: 'url',
-  //     data: { id: 0, action: 'once', key: 'method', payload: ['params'], name: 'manager' }
-  //   }
-  //   iframe['getEvent'](event as any)
-  //   expect(iframe.once.mock.calls[0][0]).toEqual('manager')
-  //   expect(iframe.once.mock.calls[0][1]).toEqual('method')
-  // })
+describe('Connector', () => {
+  let manager: PluginManager
+  let plugin: MockPlugin
 
-  // // Get Message: off
-  // test('getEvent with once should run listen only one', () => {
-  //   iframe['origin'] = 'url'
-  //   const event = {
-  //     origin: 'url',
-  //     data: { id: 0, action: 'off', key: 'method', payload: ['params'], name: 'manager' }
-  //   }
-  //   iframe['getEvent'](event as any)
-  //   expect(iframe.off.mock.calls[0][0]).toEqual('manager')
-  // })
+  beforeEach(async () => {
+    manager = new PluginManager(pluginManagerProfile)
+    const engine = new Engine(manager)
+    await engine.onload()
+    plugin = new MockPlugin()
+    engine.register(plugin)
+    plugin.createMock()
+  })
+
+  test('activation should call connect', () => {
+    plugin.activate()
+    expect(plugin.connect).toHaveBeenCalled()
+  })
+
+  test('deactivate should call disconnect', () => {
+    plugin['loaded'] = true
+    plugin.deactivate()
+    expect(plugin['loaded']).toBeFalsy()
+    expect(plugin.disconnect).toHaveBeenCalled()
+  })
+
+  // Get Message: request
+  test('[getMessage] request shoud trigger "call"', () => {
+    const message = { id: 0, action: 'request', key: 'method', payload: ['params'], name: 'manager' } as any
+    plugin['getMessage'](message)
+    expect(plugin.call).toHaveBeenCalledWith('manager', 'method', 'params')
+  })
+
+  // Get Message: listen
+  test('[getMessage] request shoud trigger "on"', () => {
+    const message = { id: 0, action: 'listen', key: 'method', payload: ['params'], name: 'manager' } as any
+    plugin['getMessage'](message)
+    expect(plugin.on.mock.calls[0][0]).toEqual('manager')
+    expect(plugin.on.mock.calls[0][1]).toEqual('method')
+
+    plugin.callMockEvent('params')
+    plugin.callMockEvent('params')
+    plugin.callMockEvent('params')
+    const response = { action: 'notification', key: 'method', payload: ['params'], name: 'manager' } as any
+    expect(plugin.send).toHaveBeenCalledWith(response)
+    expect(plugin.send).toHaveBeenCalledTimes(3)
+  })
+
+  // getMessage once
+  // We are not testing the once functionality here, this is done by the engine
+  test('[getMessage] request shoud trigger "once"', () => {
+    const message = { id: 0, action: 'once', key: 'method', payload: ['params'], name: 'manager' } as any
+    plugin['getMessage'](message)
+    expect(plugin.once.mock.calls[0][0]).toEqual('manager')
+    expect(plugin.once.mock.calls[0][1]).toEqual('method')
+  })
+
+  // getMessage off
+  test('[getMessage] request shoud trigger "once"', () => {
+    const message = { id: 0, action: 'off', key: 'method', payload: ['params'], name: 'manager' } as any
+    plugin['getMessage'](message)
+    expect(plugin.off).toHaveBeenCalledWith('manager', 'method')
+  })
+
 })
