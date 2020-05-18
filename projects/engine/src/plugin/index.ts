7aef762f7465711367435a0f4343cfb1a292c37a
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-18 14:58:44 +0200

    remove specific plugins out of main library

--- projects/engine/src/plugin/index.ts
+++ projects/engine/src/plugin/index.ts
@@ -3,7 +3,3 @@ export * from './host'
 export * from './library'
 export * from './view'
 export * from './manager'
-
-// export * from './websocket'
-// export * from './iframe'
-// export * from './child-process'
