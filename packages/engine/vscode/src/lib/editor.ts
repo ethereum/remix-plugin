91fcd9c8f5b16e770b1390e6d881b35ee976c4a7
0mkar <0mkar@protonmail.com>
2020-09-14 20:30:27 +0530

    changes against review PR #238

--- packages/engine/vscode/src/lib/editor.ts
+++ packages/engine/vscode/src/lib/editor.ts
@@ -1,6 +1,6 @@
 import { CommandPlugin } from "@remixproject/engine-vscode";
-import { Annotation, RemixPosition } from "./type";
-import { window, Range, TextEditorDecorationType, Position, languages, DiagnosticCollection, Diagnostic, Uri, DiagnosticSeverity } from "vscode";
+import { Annotation, HighlightPosition } from '@remixproject/plugin-api';
+import { window, Range, TextEditorDecorationType, Position, languages, DiagnosticCollection, Diagnostic, Uri, DiagnosticSeverity, TextEditor } from "vscode";
 
 const profile = {
   name: "editor",
@@ -19,50 +19,60 @@ export default class EditorPlugin extends CommandPlugin {
   private diagnosticCollection: DiagnosticCollection;
   constructor() {
     super(profile);
+  }
+  onActivation() {
     this.decoration = window.createTextEditorDecorationType({
       backgroundColor: 'editor.lineHighlightBackground',
       isWholeLine: true,
     });
     this.diagnosticCollection = languages.createDiagnosticCollection('solidity');
   }
-  highlight(position: RemixPosition, filePath: string, hexColor: string): void {
+  onDeactivation() {
+    this.decoration.dispose();
+  }
+  highlight(position: HighlightPosition, filePath: string, hexColor: string): void {
     const editors = window.visibleTextEditors;
     // Parse `filePath` to ensure if a valid file path was supplied
-    const editor = editors.filter(editor => editor.document.uri.path == Uri.parse(filePath).path)[0];
-    const start: Position = new Position(position.start.line, position.start.column);
-    const end: Position = new Position(position.end.line, position.end.column);
-    const newDecoration = { range: new Range(start, end) };
-    if (hexColor) {
-      this.decoration = window.createTextEditorDecorationType({
-        backgroundColor: hexColor,
-        isWholeLine: true,
-      });
+    const editor = editors.find(editor => editor.document.uri.path === Uri.parse(filePath).path);
+    if(editor) {
+      const start: Position = new Position(position.start.line, position.start.column);
+      const end: Position = new Position(position.end.line, position.end.column);
+      const newDecoration = { range: new Range(start, end) };
+      if (hexColor) {
+        this.decoration = window.createTextEditorDecorationType({
+          backgroundColor: hexColor,
+          isWholeLine: true,
+        });
+      }
+      return editor.setDecorations(this.decoration, [newDecoration]);
+    } else {
+      throw new Error(`Could not find file ${filePath}`);
     }
-    return editor.setDecorations(this.decoration, [newDecoration]);
   }
   discardHighlight(): void {
     return this.decoration.dispose();
   }
   discardHighlightAt(line: number, filePath: string): void {}
-  addAnnotation(annotation: Annotation): void {
+  private getEditor(filePath?: string): TextEditor {
+    const editors = window.visibleTextEditors;
+    return filePath ? editors.find(editor => editor.document.uri.path === Uri.parse(filePath).path) : window.activeTextEditor
+  }
+  addAnnotation(annotation: Annotation, filePath?: string): void {
     // This function should append to existing map
     // Ref: https://code.visualstudio.com/api/language-extensions/programmatic-language-features#provide-diagnostics
-    const fileUri = window.activeTextEditor ? window.activeTextEditor.document.uri : undefined; // TODO: we might want to supply path to addAnnotation function
-    let diagnosticMap: Map<string, Diagnostic[]> = new Map();
-    let canonicalFile = fileUri.toString();
-    let range = new Range(annotation.row - 1, annotation.column, annotation.row - 1, annotation.column);
-    let diagnostics = diagnosticMap.get(canonicalFile);
-    if (!diagnostics) { diagnostics = []; }
-    let svrt: DiagnosticSeverity = 0;
-    switch (annotation.type) {
-      case 'error':
-        svrt = 0;
-      case 'warning':
-        svrt = 1;
-      case 'information':
-        svrt = 2;
-    }
-    diagnostics.push(new Diagnostic(range, annotation.text, svrt));
+    // const fileUri = window.activeTextEditor ? window.activeTextEditor.document.uri : undefined; // TODO: we might want to supply path to addAnnotation function
+    const editor = this.getEditor(filePath);
+    const canonicalFile: string = editor.document.uri.fsPath;
+    const diagnosticMap: Map<string, Diagnostic[]> = new Map();
+    const range = new Range(annotation.row - 1, annotation.column, annotation.row - 1, annotation.column);
+    const diagnostics = diagnosticMap.get(canonicalFile) ? diagnosticMap.get(canonicalFile) : [];
+    const diagnosticSeverity: Record<string, DiagnosticSeverity> = {
+      'error': DiagnosticSeverity.Error,
+      'warning': DiagnosticSeverity.Warning,
+      'information': DiagnosticSeverity.Information
+    };
+    const severity = diagnosticSeverity[annotation.type];
+    diagnostics.push(new Diagnostic(range, annotation.text, severity));
     diagnosticMap.set(canonicalFile, diagnostics);
     diagnosticMap.forEach((diags, file) => {
       this.diagnosticCollection.set(Uri.parse(file), diags);
