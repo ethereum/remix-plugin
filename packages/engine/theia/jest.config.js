f0150cc683b920d7bc07bf152bee23c7a7f9907d
Florian Antony <flow.antony@googlemail.com>
2020-11-09 16:07:29 +0100

    Skeleton for engine/theia

---
+++ packages/engine/theia/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'engine-theia',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/packages/engine/theia',
+};
