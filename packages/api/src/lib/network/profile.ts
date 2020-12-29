babf0a6150f3266ad113607f16266ea52f7ac327
Your Name <you@example.com>
2020-12-29 13:23:25 +0100

    rm profile

--- packages/api/src/lib/network/profile.ts
+++
@@ -1,8 +0,0 @@
-import { INetwork } from './api'
-import { LibraryProfile } from '@remixproject/plugin-utils'
-
-export const networkProfile: LibraryProfile<INetwork> = {
-  name: 'network',
-  methods: ['addNetwork', 'detectNetwork', 'getEndpoint', 'getNetworkProvider', 'removeNetwork'],
-  events: ['providerChanged']
-}
