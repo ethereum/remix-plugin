4d97be3de414d9abfe60b362cc0c203572caab5a
Your Name <you@example.com>
2020-12-29 13:47:11 +0100

    add statusevents & profile

--- packages/api/src/lib/network/api.ts
+++ packages/api/src/lib/network/api.ts
@@ -1,10 +1,11 @@
+import { StatusEvents } from '@remixproject/plugin-utils';
 import { NetworkProvider, Network, CustomNetwork } from './type';
 
 /** @deprecated: current version in Remix IDE. To improve to match standard JSON RPC methods */
-export interface NetworkApi {
+export interface INetwork {
   events: {
     providerChanged: (provider: NetworkProvider) => void
-  }
+  } & StatusEvents
   methods: {
     getNetworkProvider(): NetworkProvider
     detectNetwork(): Network | Partial<CustomNetwork>
