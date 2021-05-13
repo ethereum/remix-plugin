4412d6fd1af32b2ccd624bd470c7fcb29c035b89
filip mertens <filip.mertens@ethereum.org>
2021-05-13 17:18:54 +0200

    update profile

--- packages/api/src/lib/file-system/file-panel/profile.ts
+++ packages/api/src/lib/file-system/file-panel/profile.ts
@@ -8,6 +8,6 @@ export const filePanelProfile: Profile<IFilePanel> & LocationProfile = {
   location: "sidePanel",
   documentation: "",
   version: "0.0.1",
-  methods: ['getCurrentWorkspace', 'getWorkspaces', 'createWorkspace', 'renameWorkspace'],
-  events: ['setWorkspace', 'renameWorkspace', 'deleteWorkspace', 'createWorkspace']
+  methods: ['getCurrentWorkspace', 'getWorkspaces', 'createWorkspace', 'renameWorkspace', 'registerContextMenuItem'],
+  events: ['setWorkspace', 'renameWorkspace', 'deleteWorkspace', 'createWorkspace', 'customAction']
 };
